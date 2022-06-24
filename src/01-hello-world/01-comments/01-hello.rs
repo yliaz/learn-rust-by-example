f26822ea282ba701491a4f211cc824c4b7b9c7d9
zhuye <zhuye256@163.com>
2022-06-24 17:57:28 +0800

    Finish 01 Hello World

---
+++ src/01-hello-world/01-comments/01-hello.rs
@@ -0,0 +1,12 @@
+// 这是单行注释
+/**
+ * 这是块注释 /* 可以嵌套 */
+ */
+
+fn main() {
+    println!("Hello World!");
+    println!("I'm Zhu Ye.");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
